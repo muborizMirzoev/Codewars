52ae580f3f6b2a7754a100ea8989acd3f1448cfd
muborizMirzoev <muboriz10@gmail.com>
2020-12-29 17:34:06 +0700

    add solution:  7 kyu

---
+++ JavaScript/7KYU/make-a-function-that-does-arithmetic.js
@@ -0,0 +1,42 @@
+// https://www.codewars.com/kata/583f158ea20cfcbeb400000a/train/javascript
+// Given two numbers and an arithmetic operator (the name of it, as a string), return the result of the two numbers having that operator used on them.
+
+// a and b will both be positive integers, and a will always be the first number in the operation, and b always the second.
+
+// The four operators are "add", "subtract", "divide", "multiply".
+
+// A few examples:
+
+// arithmetic(5, 2, "add")      => returns 7
+// arithmetic(5, 2, "subtract") => returns 3
+// arithmetic(5, 2, "multiply") => returns 10
+// arithmetic(5, 2, "divide")   => returns 2.5
+// ArithmeticFunction.arithmetic(5, 2, "add")      => returns 7
+// ArithmeticFunction.arithmetic(5, 2, "subtract") => returns 3
+// ArithmeticFunction.arithmetic(5, 2, "multiply") => returns 10
+// ArithmeticFunction.arithmetic(5, 2, "divide")   => returns 2
+
+const operators = {
+  add(a, b) {
+    return a + b
+  },
+  subtract(a, b) {
+    return a - b
+  },
+  multiply(a, b) {
+    return a * b
+  },
+  divide(a, b) {
+    return a / b
+  },
+}
+
+function arithmetic(a, b, operator) {
+  for (let key in operators) {
+    if (key === operator) {
+      return operators[key](a, b)
+    }
+  }
+};
+
+arithmetic(5, 2, "divide")
