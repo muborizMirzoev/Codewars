258c3958fc6e3e727ed561dd17dd2082e4a1eec7
muborizMirzoev <muboriz10@gmail.com>
2021-01-03 07:01:05 +0700

    add solution:  6 kyu

---
+++ JavaScript/6KYU/count-the-smiley-faces.js
@@ -0,0 +1,37 @@
+// https://www.codewars.com/kata/583203e6eb35d7980400002a/train/javascript
+
+// Given an array (arr) as an argument complete the function countSmileys that should return the total number of smiling faces.
+
+// Rules for a smiling face:
+
+// Each smiley face must contain a valid pair of eyes. Eyes can be marked as : or ;
+// A smiley face can have a nose but it does not have to. Valid characters for a nose are - or ~
+// Every smiling face must have a smiling mouth that should be marked with either ) or D
+// No additional characters are allowed except for those mentioned.
+
+// Valid smiley face examples: :) :D ;-D :~)
+// Invalid smiley faces: ;( :> :} :]
+
+// Example
+// countSmileys([':)', ';(', ';}', ':-D']);       // should return 2;
+// countSmileys([';D', ':-(', ':-)', ';~)']);     // should return 3;
+// countSmileys([';]', ':[', ';*', ':$', ';-D']); // should return 1;
+// Note
+// In case of an empty array return 0. You will not be tested with invalid input (input will always be an array). Order of the face (eyes, nose, mouth) elements will always be the same.
+
+//Можно решить регулярками, но я пока не изучил их.
+function countSmileys(arr) {
+  return arr.filter((item) => {
+    if (item.length > 3) return false
+    if ((item.includes(':') || item.includes(';')) && (item.includes(')') || item.includes('D')) ) {
+      if ((item.length > 2) && (item.includes('-') || item.includes('~'))) {
+        return item
+      } else if (item.length === 2) {
+        return item
+      }
+    }
+  }).length;
+}
+
+countSmileys([ ':-)', ';~D', ':-D', ':_D', ':D' ]);
+
